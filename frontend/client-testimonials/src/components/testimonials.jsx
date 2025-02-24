a4fa1d15cab3c5e675ed817092e17f09a207cca4
Monish892 <jayachitraramesh08@gmail.com>
2025-02-24 15:24:19 +0530

    files added

--- frontend/client-testimonials/src/components/testimonials.jsx
+++ frontend/client-testimonials/src/components/testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Testimonialcard({name,review}){
     return(
-        <div style={{border:'1px solid white',padding:'20px',margin:'10px',borderRadius:'8px'}}>
+        <div style={{border:'1px solid black',padding:'20px',margin:'10px',borderRadius:'8px',width:'200px'}}>
             <h3>{name}</h3>
             <p>{review}</p>
         </div>
